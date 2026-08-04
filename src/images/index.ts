66aa5c8cf9d00942bffff04090ccc101939a2953
sk5314624-droid <sk5314624@gmail.com>
2026-08-04 16:53:13 +0600

    Update index.ts

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,19 +1,19 @@
-import hamsterExchange from "./hamster-exchange.png";
+import TradeCoin(TDC)Exchange from "./TradeCoin(TDC)-exchange.png";
 import binanceLogo from "./binance-logo.png";
 import dollarCoin from "./dollar-coin.png";
 import dailyReward from "./daily-reward.png";
 import dailyCipher from "./daily-cipher.png";
 import dailyCombo from "./daily-combo.png";
 import mainCharacter from "./main-character.png";
-import hamsterCoin from "./hamster-coin.png";
+import TradeCoin from "./Trade-coin.png";
 
 export {
-    hamsterExchange,
+    TradeCoin(TDC)Exchange,
     binanceLogo,
     dollarCoin,
     dailyReward,
     dailyCipher,
     dailyCombo,
     mainCharacter,
-    hamsterCoin
+    TradeCoin
 };
